4c04bdad22c2e7767ede6dfe71c27dab53f6aaa4
BAÑO GUERRERO OSWALDO VALENTIN <ovbano@espe.edu.ec>
2025-05-05 20:13:46 -0500

    Versión 9

--- php/conexionM.php
+++ php/conexionM.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'vendor/autoload.php';
     $client = new MongoDB\Client(
-        // 'mongodb://localhost:27017'
-        'mongodb+srv://ovbano:FJ2p7V458wHugg1a@cluster0.0lkuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+        'mongodb://localhost:27017'
+        // 'mongodb+srv://ovbano:FJ2p7V458wHugg1a@cluster0.0lkuh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     );
 ?>
